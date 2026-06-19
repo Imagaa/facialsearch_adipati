2d3e075e8162f1ce05910010b828672613a491bc
Lokesvara . <ivanwahyu122@gmail.com>
2026-06-19 12:52:15 +0700

    fix CORS problem

--- api_server.py
+++ api_server.py
@@ -15,7 +15,7 @@ app_api = FastAPI()
 app_api.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False, # HARAM bernilai True jika origins menggunakan wildcard "*" di server produksi
     allow_methods=["*"],
     allow_headers=["*"],
 )
